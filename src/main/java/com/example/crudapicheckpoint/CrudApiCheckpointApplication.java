b7db876107707990931578f2e33cda95aa169637
Joseph Myrick <joseph.l.myrick2.mil@swf.army.mil>
2021-09-27 14:14:04 -0500

    Initial commit.

---
+++ src/main/java/com/example/crudapicheckpoint/CrudApiCheckpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudapicheckpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiCheckpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiCheckpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudapicheckpoint/CrudApiCheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudapicheckpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiCheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
